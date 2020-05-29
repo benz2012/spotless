e58066fd99b664c189ca0253fbb759148124170f
Ben Zenker <imbenzenker@gmail.com>
2020-05-28 23:25:01 -0400

    Began api integration

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+axios.defaults.headers.common['Authorization'] = 'Bearer ...'
+
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Sort your music',
+    message: 'Spotless: Clean up your music',
+    user_id: 'enigmacubed',
+    playlists: [],
+    tracks: [],
+  },
+  methods: {
+    getPlaylists: function () {
+      axios
+        .get(`https://api.spotify.com/v1/users/${this.user_id}/playlists`)
+        .then(response => (this.playlists = response.data.items))
+    },
+    getTracksForPlaylist: function (playlist_id) {
+      axios
+        .get(`https://api.spotify.com/v1/playlists/${playlist_id}/tracks`)
+        .then(response => (
+          this.tracks = response.data.items
+            .map(item => item.track)
+            .filter(item => item.type === 'track')
+            .map(item => Object.assign({}, item, {
+              artists: item.artists.map(artist => artist.name).join(', '),
+            }))
+        ))
+    },
   },
 })
